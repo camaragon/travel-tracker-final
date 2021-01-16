b2f9edcbcf2bb0fdaf11cb51b0d1c02a5701c635
Cameron Aragon <caragon4695@gmail.com>
2021-01-16 15:31:49 -0700

    Add testing for all properties in the trip class

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -18,4 +18,36 @@ describe('Trip', () => {
     it('should be an instance of Trip', () => {
         expect(trip).to.be.an.instanceof(Trip);
     });
+
+    it('should initialize with an id', () => {
+        expect(trip.id).to.eq(1);
+    });
+
+    it('should initialize with a user id', () => {
+        expect(trip.userID).to.eq(44);
+    });
+
+    it('should initialize with a destination id', () => {
+        expect(trip.destinationID).to.eq(49);
+    });
+
+    it('should initialize with a number of travelers', () => {
+        expect(trip.travelers).to.eq(1);
+    });
+
+    it('should initialize with a date', () => {
+        expect(trip.date).to.eq("2019/09/16");
+    });
+
+    it('should initialize with a duration', () => {
+        expect(trip.duration).to.eq(8);
+    });
+
+    it('should initialize with a status', () => {
+        expect(trip.status).to.eq("approved");
+    });
+
+    it('should initialize with suggested activities', () => {
+        expect(trip.suggestedActivities).to.eql([]);
+    });
 });
