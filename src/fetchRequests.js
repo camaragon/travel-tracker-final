aebcac8901ac3a4d06bba243ecf4a4ab9bd7bfda
Cameron Aragon <caragon4695@gmail.com>
2021-01-17 18:52:01 -0700

    Finish fetch setup and promise.all

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -1,28 +1,4 @@
 const fetchRequests = {
-    getTravelers() {
-      return fetch("http://localhost:3001/api/v1/travelers")
-      .then(response => {
-        if (!response.ok) {
-          throw Error(`${response.status} ${response.statusText}`);
-        }
-        return response;
-      })
-      .then(response => response.json())
-      .catch(error => alert(error));
-    },
-  
-    getTrips() {
-      return fetch("http://localhost:3001/api/v1/trips")
-      .then(response => {
-        if (!response.ok) {
-          throw Error(`${response.status} ${response.statusText}`);
-        }
-        return response;
-      })
-      .then(response => response.json())
-      .catch(error => alert(error));
-    },
-  
     getDestinations() {
       return fetch("http://localhost:3001/api/v1/destinations")
       .then(response => {
@@ -33,5 +9,31 @@ const fetchRequests = {
       })
       .then(response => response.json())
       .catch(error => alert(error));
-    }
+    },
+    
+    getTrips() {
+        return fetch("http://localhost:3001/api/v1/trips")
+        .then(response => {
+            if (!response.ok) {
+                throw Error(`${response.status} ${response.statusText}`);
+            }
+            return response;
+        })
+        .then(response => response.json())
+        .catch(error => alert(error));
+    },
+
+    getTravelers(id) {
+        return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(`${response.status} ${response.statusText}`);
+        }
+        return response;
+    })
+    .then(response => response.json())
+    .catch(error => alert(error));
 }
+};
+
+export default fetchRequests;
